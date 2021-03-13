4ed91828bcc5885b4f9186fb93970b6e07a4f9ff
amatt <amattson84@gmail.com>
2021-03-12 20:30:23 -0800

    Created table: role

--- schema.sql
+++ schema.sql
@@ -7,3 +7,9 @@ CREATE TABLE department(
   id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   department_name VARCHAR(30)
 );
+CREATE TABLE role(
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(100),
+  salary DECIMAL,
+  department_id int
+);
