56741e775e2cc56e85ae48e57aaa4fbefb809c2d
amatt <amattson84@gmail.com>
2021-03-12 20:32:02 -0800

    Seeds for  table date for department, role and employee

--- seeds.sql
+++ seeds.sql
@@ -14,8 +14,3 @@ INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ('Patrick', 'Schwayze', '3333', 456);
 
 
-SELECT * FROM employee;
--- INSERT INTO department (id, name) values ('1234', 'Music');
-SELECT * FROM department;
--- INSERT INTO role (id, title, salary, department_id) values ('1','singer','987654');
-SELECT * FROM role;
